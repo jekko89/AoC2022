bf0242f9493673b4b42fc7b21f24ec6d5ca3d50c
jekko89 <jon_eco@yahoo.com>
2023-04-24 08:38:27 -0700

    Day 6 Added
    Part 1 and 2 for Day 6 - input buffers - added

---
+++ day6/2022-day6-1.py
@@ -0,0 +1,29 @@
+#open input file
+file = open('input.txt', 'r')
+
+#inputs
+for line in file.readlines():
+    #create 4 character buffer
+    buffer = []
+    bufferMax = 4
+    indexFound = True
+
+    #create marker variable
+    marker = 0
+
+    #ensure buffer is at 4 characters
+    for item in line:
+        buffer.append(item)
+        marker += 1
+        if (len(buffer) == bufferMax):
+            for x in buffer:
+                if buffer.count(x) > 1:
+                    indexFound = False
+                    break
+            if indexFound == True:
+                #return index and break out of loop
+                print(marker)
+                break
+            #clean up for next loop
+            indexFound = True
+            buffer.pop(0)
